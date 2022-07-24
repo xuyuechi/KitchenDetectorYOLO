dcc26f7677a55c0b8c3b963132e519694ad686f2
xuyuechi <xuyuechi@m.scnu.edu.cn>
2022-07-24 13:36:07 +0800

    乱码问题解决

--- src/main/java/org/kitchenDet/filter/CharacterEncodingFilter.java
+++ src/main/java/org/kitchenDet/filter/CharacterEncodingFilter.java
@@ -20,7 +20,7 @@ public class CharacterEncodingFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        //response.setContentType("text/html;charset=utf-8");
+        //response.setContentType("text/html;charset=UTF-8");
         chain.doFilter(request,response);
 
     }
